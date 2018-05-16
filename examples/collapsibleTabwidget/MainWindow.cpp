1d6697a8f46b0ef93d854c3010365f9de934d262
Filiberto Zaá Avila <filizaa@gmail.com>
2018-05-15 22:38:33 -0400

    Limpieza del código

--- examples/collapsibleTabwidget/MainWindow.cpp
+++ examples/collapsibleTabwidget/MainWindow.cpp
@@ -12,10 +12,3 @@ MainWindow::MainWindow ( QWidget *parent ) : QMainWindow ( parent ) {
 
   this->setCentralWidget ( collapsibleTabWidget );
 }
-
-//TODO: Adaptar el código para poder ser utilizado desde QtDesigner.
-//TODO: Adaptar el código a qt-plugin-system.
-//TODO: Hacer de forma transparente, que al colapsar el QTabWidget se redimensione su contenedor padre.
-// Al colapsar el QTabWidget, permitir se redimensione el contenedor padre no está implementado aun.
-// TODO: Documentar el código.
-// TODO: Hasta ahora hay que definir un MaximunHeight y un tamaño inicial igual a MaximunHeight para evitar discrepancias en la animación
